14fcff80ce767075a6208f9220af183d372e022e
thacharayil <thacharayila@gmail.com>
2024-01-19 07:28:21 +0000

    Test Commit 2 by Akhil T

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel created by Akhil T</title>
+        <title>Akhil T</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
